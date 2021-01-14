630f92fc464fad7ea3b459cfd374d59a3842f93a
mdziedzic97 <mdziedzic97@gmail.com>
2021-01-14 19:37:12 +0100

    First try at express and postman controling apis

---
+++ Users.js
@@ -0,0 +1,25 @@
+const users = [
+    {
+        id: 1,
+        name: "John",
+        email: "John@gmail.com"
+    },
+    {
+        id: 2,
+        name: "Mark",
+        email: "Mark@gmail.com"
+    },
+    {
+        id: 3,
+        name: "Paige",
+        email: "Paige@gmail.com"
+    },
+    {
+        id: 4,
+        name: "Anna",
+        email: "Anna@gmail.com"
+    },
+
+]
+
+module.exports = users
